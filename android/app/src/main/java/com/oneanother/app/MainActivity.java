6f21495be429239c6ffe131d574fc3f5ee9081f6
Anthony Saleeb <anthonysaleeb@gmail.com>
2026-04-03 16:02:45 +1100

    fixed logout issues

--- android/app/src/main/java/com/oneanother/app/MainActivity.java
+++ android/app/src/main/java/com/oneanother/app/MainActivity.java
@@ -1,5 +1,16 @@
 package com.oneanother.app;
 
+import android.webkit.CookieManager;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onStop() {
+        super.onStop();
+        // Flush the WebView CookieManager to disk so that cookie deletions
+        // (e.g. on logout) are persisted before the process is killed.
+        // Without this, the session cookie survives a cold app restart even
+        // after the server has cleared it via Set-Cookie: Max-Age=0.
+        CookieManager.getInstance().flush();
+    }
+}
